646ea1ef4a99289645b91a911e06e3946687a021
Aluminogenese <106045679+Aluminogenese@users.noreply.github.com>
2023-11-28 12:59:46 +0800

    Update control_point.h

--- AnalyticalPhotogrammetry/control_point.h
+++ AnalyticalPhotogrammetry/control_point.h
@@ -8,8 +8,10 @@ public:
 	ControlPoint();
 	~ControlPoint();
 	ControlPoint(double x, double y, double z);
+	ControlPoint(int id, double x, double y, double z);
 
 public:
+	int id_;
 	double x_;
 	double y_;
 	double z_;
