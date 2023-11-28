4b7e93bf5bff21a090df90827cb4b295be9b81b0
Aluminogenese <106045679+Aluminogenese@users.noreply.github.com>
2023-11-28 13:00:37 +0800

    Update image_point.cpp

--- AnalyticalPhotogrammetry/image_point.cpp
+++ AnalyticalPhotogrammetry/image_point.cpp
@@ -2,6 +2,7 @@
 
 ImagePoint::ImagePoint()
 {
+	id_ = 0;
 	x_ = 0;
 	y_ = 0;
 }
@@ -12,6 +13,13 @@ ImagePoint::~ImagePoint()
 
 ImagePoint::ImagePoint(double x, double y)
 {
+	x_ = x;
+	y_ = y;
+}
+
+ImagePoint::ImagePoint(int id, double x, double y)
+{
+	id_ = id;
 	x_ = x;
 	y_ = y;
 }
